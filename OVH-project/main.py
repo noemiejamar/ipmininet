93f5bf6a4f56dd0624a2f2b54dedd90589ab862b
LouisDeVogeleer <38432465+LouisDeVogeleer@users.noreply.github.com>
2020-11-13 15:46:49 +0100

    update DNS network

--- OVH-project/main.py
+++ OVH-project/main.py
@@ -222,7 +222,7 @@ class SimpleBGPTopo(IPTopo):
         # --- DNS network---
         domain = "ovh.com"
         # ipv4_server_address = '139.99.0.50'  # routerID starts at 50 for hosts
-        ipv6_server_address = 'babe:1:10:5500::'
+        ipv6_server_address = 'babe:1:10:5000::'
 
         # Add hosts
 
@@ -233,7 +233,7 @@ class SimpleBGPTopo(IPTopo):
         master = self.addHost('master')
         master.addDaemon(Named)
         self.addLink(as1_bb1, master, igp_metric=1, params1={"ip": "BABE:1:10:0306::/64"},
-                     params2={"ip": "BABE:1:10:5400::/64"})
+                     params2={"ip": "BABE:1:10:5100::/64"})
 
         slave = self.addHost('slave')
         slave.addDaemon(Named)
