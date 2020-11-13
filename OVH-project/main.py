c56ed10160d7b2dcf3ecb4a182f6ac284778d1ac
LouisDeVogeleer <38432465+LouisDeVogeleer@users.noreply.github.com>
2020-11-13 16:29:00 +0100

    update DNS network

--- OVH-project/main.py
+++ OVH-project/main.py
@@ -231,12 +231,16 @@ class SimpleBGPTopo(IPTopo):
                      params2={"ip": "BABE:1:10:5000::/64"})
 
         master = self.addRouter('master')
-        master.addDaemon(Named).addDaemon(OSPF).addDaemon(OSPF6)
+        master.addDaemon(Named)
+        master.addDaemon(OSPF)
+        master.addDaemon(OSPF6)
         self.addLink(as1_bb1, master, igp_metric=1, params1={"ip": "BABE:1:10:0306::/64"},
                      params2={"ip": "BABE:1:10:5100::/64"})
 
         slave = self.addRouter('slave')
-        slave.addDaemon(Named).addDaemon(OSPF).addDaemon(OSPF6)
+        slave.addDaemon(Named)
+        slave.addDaemon(OSPF)
+        slave.addDaemon(OSPF6)
         self.addLink(as1_bb2, slave, igp_metric=1, params1={"ip": "BABE:1:10:0408::/64"},
                      params2={"ip": "BABE:1:10:5100::/64"})
 
