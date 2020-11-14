78e9acae0a6392ae2faacd57463c2da50069c4a7
allamim <43554855+allamim@users.noreply.github.com>
2020-11-14 18:25:30 +0100

    Update firewall.py

--- OVH-project/firewall.py
+++ OVH-project/firewall.py
@@ -28,13 +28,13 @@ ip6_rules = [
             # Allow eBGP from any client towards our ebgp router-interfaces
 
             # (telstra3,as1_r15)
-            Rule('-A INPUT --src babe:1:11:1600::/64 -d 2001:1:11:1401::/64 -j ACCEPT'),
+            Rule('-A INPUT --src 2001:1:11:1600::/64 -d 2001:1:11:1401::/64 -j ACCEPT'),
             # (ntt3,as1_r14)
-            Rule('-A INPUT --src babe:1:11:1900::/64 -d 2001:1:11:1301::/64 -j ACCEPT'),
+            Rule('-A INPUT --src 2001:1:11:1900::/64 -d 2001:1:11:1301::/64 -j ACCEPT'),
             # (equinix2,as1_r14)
-            Rule('-A INPUT --src babe:1:11:1800::/64 -d 2001:1:11:1302::/64 -j ACCEPT'),
+            Rule('-A INPUT --src 2001:1:11:1800::/64 -d 2001:1:11:1302::/64 -j ACCEPT'),
             # (telstra1,as1_bb1)
-            Rule('-A INPUT --src babe:1:10:0800::/64 -d 2001:1:10:0302::/64 -j ACCEPT'),
+            Rule('-A INPUT --src 2001:1:10:0800::/64 -d 2001:1:10:0302::/64 -j ACCEPT'),
             # (ntt1,as1_bb1)
             Rule('-A INPUT --src 2001:1:10:0900::/64 -d 2001:1:10:0303::/64 -j ACCEPT'),
             # (telstra2,as1_bb2)
