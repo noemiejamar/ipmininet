9de37eeb29b0b23e2df38f34b6e5e4522546165d
allamim <43554855+allamim@users.noreply.github.com>
2020-12-15 00:42:36 +0100

    Update syd_r3_FRRoutingCMD.py

--- OVH-project/syd_r3_FRRoutingCMD.py
+++ OVH-project/syd_r3_FRRoutingCMD.py
@@ -24,7 +24,7 @@ bgp_FRRouting.sendline('exit')
 
 # Filter regions
 
-bgp_FRRouting.sendline('bgp community-list standard FILTER-AUS-ipv6 deny 16276:1501')
+bgp_FRRouting.sendline('bgp community-list standard FILTER-AUS-ipv6 deny 16276:151')
 bgp_FRRouting.sendline('bgp community-list standard FILTER-AUS-ipv6 permit internet')
 
 bgp_FRRouting.sendline('route-map rm0-in-ipv6 permit 10')
@@ -38,3 +38,4 @@ bgp_FRRouting.sendline('exit')
 time.sleep(0.1)
 
 bgp_FRRouting.kill(0)
+
